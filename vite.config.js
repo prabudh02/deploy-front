43f0f5d9714de17dcb5974fea84efdfebbac8b70
Prabudh tomar <prabudhtomar@Prabudhs-MacBook-Air.local>
2025-05-27 18:25:39 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // Adjust this to your backend server URL
+        target: 'https://server-pearl-pi.vercel.app/', // Adjust this to your backend server URL
         
       },
     },
